83e7885ac0d787950c4dcc2a42ea1cc82f34fefb
Zhiming Ni <1078830088@qq.com>
2023-02-12 00:38:56 +0800

    feat(*): add ContextMenu

--- main.js
+++ main.js
@@ -1,23 +1,17 @@
-const articleEl = document.querySelector("article");
-
-// `document.querySelector` may return null if the selector doesn't match anything.
-if (articleEl) {
-  const text = articleEl.textContent;
-  const wordReg = /[^\s]+/g;
-  const words = text.matchAll(wordReg);
-  // matchAll returns an iterator, convert to array to get word count
-  const wordNum = [...words].length;
-  const readingTime = Math.round(wordNum / 200);
-  const timeInfoEl = document.createElement("p");
-  // Use the same styling as the publish information in an article's header
-  timeInfoEl.classList.add("color-secondary-text", "type--caption");
-  timeInfoEl.textContent = `⏱️ ${readingTime} min read`;
-
-  // Support for API reference docs
-  const heading = articleEl.querySelector("h1");
-  // Support for article docs with date
-  const date = articleEl.querySelector("time")?.parentNode;
-
-  (date ?? heading).insertAdjacentElement("afterend", timeInfoEl);
-  alert(`⏱️ ${readingTime} min read`)
+const initTranslate = () => {
+  const CONTEXT_MENU_ID = "Translate_v1";
+  const translateSelected =  (info, tab) => {
+    if (info.menuItemId !== CONTEXT_MENU_ID) {
+      return;
+    }
+    console.log("Selected:\n" + info.selectionText);
+  }
+  chrome.contextMenus.create({
+    title: "%s",
+    contexts:["selection"],
+    id: CONTEXT_MENU_ID
+  });
+  chrome.contextMenus.onClicked.addListener(translateSelected);
 }
+
+initTranslate();
